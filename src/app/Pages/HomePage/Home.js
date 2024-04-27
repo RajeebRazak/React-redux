1c2202bb95b47a77359376986f5a6611050621cd
Rajeeb Razak <rajeebrazak786@gmail.com>
2024-04-27 11:46:37 +0530

    first

---
+++ src/app/Pages/HomePage/Home.js
@@ -0,0 +1,19 @@
+import React, { Fragment } from "react";
+import TopNavbar from "../../components/Header/TopNavbar";
+import HeaderSlider from "../../components/Slider/HeaderSlider";
+import AllCategories from "../../components/Categories/AllCategories";
+import LatestProducts from "../../components/Product/LatestProducts";
+import Footer from "../../components/Footer/Footer";
+function Home() {
+  return (
+    <Fragment>
+      <TopNavbar />
+      <HeaderSlider />
+      <AllCategories />
+      <LatestProducts />
+      <Footer />
+    </Fragment>
+  );
+}
+
+export default Home;
